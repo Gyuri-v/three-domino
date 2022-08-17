54c970c718b747ef3c9ad0f4906ff5c6196a05f1
Gyuri-v <fnffkdll@naver.com>
2022-08-17 18:03:36 +0900

    도미노 물리엔진 적용 1차완료

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ class Domino {
 
         this.index = info.index;
         
-        this.width = info.width || 0.6;
-        this.height = info.height || 1;
+        this.width = info.width || 1;
+        this.height = info.height || 2;
         this.depth = info.depth || 0.2;
         
         this.x = info.x || 0;
-        this.y = info.y || 0.5;
+        this.y = info.y || 1;
         this.z = info.z || 0;
 
         this.rotationX = info.rotationX || 0;
@@ -24,7 +24,7 @@ class Domino {
         this.rotationZ = info.rotationZ || 0;
 
         this.mesh = new THREE.Mesh(info.geometry, info.material);
-        this.meshName = `${this.index}번째 도미노`;
+        this.mesh.name = `${this.index}번째 도미노`;
         this.mesh.castShadow = true;
         this.mesh.position.set(this.x, this.y, this.z);
         this.mesh.rotation.set(this.rotationX, this.rotationY, this.rotationZ);
@@ -40,12 +40,15 @@ class Domino {
             mass: 1,
             position: new CANNON.Vec3( this.x, this.y, this.z ),
             shape,
+            material: dominoMaterial,
         });
 
         this.cannonBody.quaternion.setFromAxisAngle(
             new CANNON.Vec3(0, 1, 0),
             this.rotationY,
         );
+
+        this.mesh.cannonBody = this.cannonBody;
 
         this.cannonWorld.addBody(this.cannonBody)
     }
@@ -59,7 +62,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas,
     antialias: true,
 })
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(document.body.clientWidth, window.innerHeight);
 
 // scene
 const scene = new THREE.Scene;
@@ -68,7 +71,7 @@ scene.background = new THREE.Color('#222');
 // camera
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    document.body.clientWidth / window.innerHeight,
     0.1,
     1000
 )
@@ -80,16 +83,14 @@ scene.add( camera );
 const controls = new OrbitControls( camera, renderer.domElement );
 
 // AxesHelper
-const axesHelper = new THREE.AxesHelper(20);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(20);
+// scene.add(axesHelper);
 
 // gird
 const gridHelper = new THREE.GridHelper(60);
 scene.add(gridHelper);
 
 // light
-const ambientLight = new THREE.AmbientLight('#fff', 0.2);
-
 const spotLightDistanse = 15;
 const spotLight1 = new THREE.SpotLight('#fff', 1);
 spotLight1.castShadow = true;
@@ -113,7 +114,53 @@ gui.add(camera.position, 'z', -20, 20, 0.01).name('camera z');
 
 // cannon
 const cannonWorld = new CANNON.World();
-cannonWorld.gravity.set(0, -10, 0);
+cannonWorld.gravity.set(0, -50, 0);
+// 성능
+cannonWorld.broadphase = new CANNON.SAPBroadphase(cannonWorld);
+
+const defaultMaterial = new CANNON.Material('default');
+const dominoMaterial = new CANNON.Material('domino');
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.5,
+        restitution: 0.2,
+    }
+)
+cannonWorld.defaultContactMaterial = defaultContactMaterial;
+const dominoDefaultContactMaterial = new CANNON.ContactMaterial(
+    dominoMaterial,
+    defaultMaterial,
+    {
+        friction: 0.5, 
+        restitution: 0.2, 
+    }
+);
+cannonWorld.addContactMaterial(dominoDefaultContactMaterial);
+const dominoDominoContactMaterial = new CANNON.ContactMaterial(
+    dominoMaterial,
+    dominoMaterial,
+    {
+        friction: 0.5, 
+        restitution: 1.5, 
+    }
+);
+cannonWorld.addContactMaterial(dominoDominoContactMaterial);
+
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body({
+    mass: 0,
+    position: new CANNON.Vec3(0, 0, 0),
+    shape: floorShape,
+    material: defaultMaterial,
+});
+floorBody.quaternion.setFromAxisAngle(
+    new CANNON.Vec3(-1, 0, 0),
+    Math.PI / 2,
+);
+cannonWorld.addBody(floorBody);
+
 
 // mesh
 const floorGeometry = new THREE.BoxGeometry(60, 1, 60);
@@ -121,40 +168,56 @@ const floorMaterial = new THREE.MeshStandardMaterial({
     color: '#111',
 })
 const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+floorMesh.position.set(0, -1, 0);
 floorMesh.receiveShadow = true;
 floorMesh.castShadow = true;
-floorMesh.position.set(0, -1, 0);
 scene.add(floorMesh);
 
-const geometry = new THREE.BoxGeometry(1, 1.5, 0.2);
+const geometry = new THREE.BoxGeometry(1, 2, 0.2);
 const material = new THREE.MeshStandardMaterial({
     color: '#aaa',
 });
+const materialRed = new THREE.MeshStandardMaterial({
+    color: 'red',
+});
 
+let domino;
+const dominos = [];
 const tp = [19, 37, 74, 111, 150];
-// const tpv = [19, 37];
 const dominoPoints = function (i) {
     if ( i < tp[0] ){ return [0, 0, -i]; } 
-    else if ( i >= tp[0] && i < tp[1] ) { return [(i - tp[0] + 0.5), 0, -tp[0]] }
+    else if ( i == tp[0] ) { return [0.5, 0, -18.7] }
+    else if ( i == tp[1] ) { return [17.7, 0, -18.5] }
+    else if ( i == tp[2] ) { return [18, 0, 18.5] }
+    else if ( i == tp[3] ) { return [-18.7, 0, 18.5] }
+    else if ( i >= tp[0] && i < tp[1] ) { return [(i - tp[0] + 0.2), 0, -tp[0]] }
     else if ( i >= tp[1] && i < tp[2] ) { return [(tp[1] - tp[0]), 0, (-tp[0] - tp[1] + i + 0.5)] }
     else if ( i >= tp[2] && i < tp[3] ) { return [(tp[0] + tp[2] - i - 1 ), 0, tp[0]] }
     else if ( i >= tp[3] && i < tp[4] ) { return [-tp[0], 0, tp[0] + tp[3] - i - 0.5]}
     else { return 'else' }
 }
-const dominos = [];
-let domino;
 for (let i = 0; i < tp[tp.length - 1]; i++) {
-
-    const rotation = i >= tp[0] && i < tp[1] || i >= tp[2] && i < tp[3] ? Math.PI / 2 : 0;
+    let rotationY = i >= tp[0] && i < tp[1] || i >= tp[2] && i < tp[3] ? Math.PI / 2 : 0;
+    rotationY = 
+        i == 18    ? -Math.PI / 8 :
+        i == tp[0] ? -Math.PI / 4 : 
+        i == 36    ?  Math.PI * 0.4 :
+        i == tp[1] ?  Math.PI * 0.2 :
+        i == tp[2] ? -Math.PI / 4 :
+        i == tp[3] ?  Math.PI / 4 :
+        rotationY;
+    const itemMaterial = 
+    i == 18 || i == 19 || i == 36 || i == 37 || i == 74 || i == 111
+        ? materialRed : material;
 
     domino = new Domino({
         scene,
         cannonWorld,
         x: dominoPoints(i)[0],
         z: dominoPoints(i)[2],
-        geometry: geometry,
-        material: material,
-        rotationY: rotation,
+        geometry,
+        material: itemMaterial,
+        rotationY,
         index: i,
     });
     dominos.push(domino);
@@ -166,21 +229,70 @@ for (let i = 0; i < tp[tp.length - 1]; i++) {
 
 
 
-
 // 그리기
+const clock = new THREE.Clock();
+
 const draw = function () {
+    const delta = clock.getDelta();
+
+    let cannonStepTime = 1 / 60;
+    if ( delta < 0.01 ) cannonStepTime = 1 / 120;
+
+    cannonWorld.step(cannonStepTime, delta, 3);
+
+    dominos.forEach((item) => {
+        if( item.cannonBody ){
+            item.mesh.position.copy(item.cannonBody.position);
+            item.mesh.quaternion.copy(item.cannonBody.quaternion);
+        }
+    });
+    
     controls.update();
 
     renderer.render(scene, camera);
-
-    dominos.forEach((item) => {
-        item.mesh.position.copy(item.cannonBody.position);
-        item.mesh.quaternion.copy(item.cannonBody.quaternion);
-    })
-
-    window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
 }
 draw();
 
+
+// setSize
+function setSize() {
+    camera.aspect = document.body.clientWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(document.body.clientWidth, window.innerHeight);
+    renderer.render(scene, camera);
+}
+
+
+// raycaster
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function checkIntersects() {
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    for (const item of intersects) {
+        if ( intersects[0].object.cannonBody ){
+
+            const forceX = camera.position.x < 0 ? 300 : camera.position.x > -30 && camera.position.x < 30 ? 0 : -300;
+            const forceZ = camera.position.z < 0 ? 300 : -300;
+
+            item.object.cannonBody.applyForce(
+                new CANNON.Vec3(forceX, 0, forceZ),
+                // new CANNON.Vec3(0, 0, 0),
+            );
+            break;
+        };
+    }
+}
+canvas.addEventListener('click', (e) => {
+    mouse.x = (e.clientX / canvas.clientWidth) * 2 - 1;
+    mouse.y = -((e.clientY / canvas.clientHeight) * 2 - 1);
+
+    checkIntersects();
+});
+window.addEventListener('resize', setSize);
 
 
